2c8ca3cdea2997400e1dfafa7249649cfa219ce0
Paul Wright <pwright@redhat.com>
2025-12-06 15:40:55 +0000

    help and logo

--- svelte/src/components/ShortcutHelp.svelte
+++ svelte/src/components/ShortcutHelp.svelte
@@ -11,11 +11,22 @@
   <div class="shortcut-help__panel" tabindex="-1">
     <div class="shortcut-help__header">
       <div class="shortcut-help__title">
-        <h2 id="shortcutHelpTitle">Keyboard shortcuts</h2>
-        <p class="shortcut-help__subtitle">Stay in flow with navigation, editing, and quick exports.</p>
+        <h2 id="shortcutHelpTitle">Help</h2>
       </div>
       <button id="shortcutHelpClose" class="shortcut-help__close" type="button" aria-label="Close keyboard shortcuts">&times;</button>
     </div>
-    <div id="shortcutHelpList" class="shortcut-help__list" aria-live="polite"></div>
+    <section class="shortcut-help__section">
+      
+      <ul class="shortcut-help__tips">
+        <li>Play around with the example maps.</li>
+        <li>Read about <a href="https://www.wardleymaps.com" target="_blank" rel="noreferrer noopener">Wardley Maps</a>.</li>
+        <li>Copy this URL and use &quot;Load URL&quot;: <a href="https://gist.githubusercontent.com/pwright/86dbb074b35d6d17671bf17ecbf1a824/raw/git.bs" target="_blank" rel="noreferrer noopener">https://gist.githubusercontent.com/pwright/86dbb074b35d6d17671bf17ecbf1a824/raw/git.bs</a></li>
+        <li>Edit maps by moving items with Shift + Arrow keys.</li>
+      </ul>
+    </section>
+    <section class="shortcut-help__section">
+      <h3 class="shortcut-help__section-title">Keyboard shortcuts</h3>
+      <div id="shortcutHelpList" class="shortcut-help__list" aria-live="polite"></div>
+    </section>
   </div>
 </div>
